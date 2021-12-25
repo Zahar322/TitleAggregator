a6020a1e0ceb6287ad689b8958824ed5e15c1979
Zahar <pitchblease97@yandex.by>
2021-12-26 00:36:19 +0300

    remove-log-charset

--- src/main/java/com/title/aggregator/controller/BotController.java
+++ src/main/java/com/title/aggregator/controller/BotController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.nio.charset.Charset;
-
 import static com.title.aggregator.utils.Constants.MDCKeys.CHAT_ID;
 import static com.title.aggregator.utils.TelegramUtils.getChatId;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -33,7 +31,6 @@ public class BotController {
     public BotApiMethod receiveUpdate(@RequestBody Update update) {
         MDC.put(CHAT_ID, getChatId(update).toString());
         log.info(mapper.writeValueAsString(update));
-        log.info(String.valueOf(Charset.defaultCharset()));
         return botService.updateReceived(update);
     }
 }
